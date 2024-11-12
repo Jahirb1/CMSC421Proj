f3c827a77cf44d2f83baf35a3a170720f079376a
Jahirb1 <110182281+Jahirb1@users.noreply.github.com>
2024-11-11 23:38:27 -0500

    Update app.c

--- IPCP1/app.c
+++ IPCP1/app.c
@@ -93,3 +93,30 @@ void run_application(int id, struct temp_buffer *temp_buffers) {
     printf("Application %d exiting\n", app_id + 1);
 }
 
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <app_id>\n", argv[0]);
+        exit(1);
+    }
+    int app_id = atoi(argv[1]);  // Convert the app_id argument from string to integer
+    printf("App started with app_id: %d\n", app_id);
+
+    // Attach to the shared memory segment created in main.c
+    int shm_fd = shm_open("/temp_buffers", O_RDWR, 0666);  // Open the shared memory region
+    if (shm_fd == -1) {
+        perror("shm_open");
+        exit(1);
+    }
+
+    shared_temp_buffers = mmap(0, NUM_APPS * sizeof(struct temp_buffer), 
+                               PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (shared_temp_buffers == MAP_FAILED) {
+        perror("mmap");
+        exit(1);
+    }
+
+    // Run the application logic using the app_id
+    run_application(app_id, shared_temp_buffers);
+
+    return 0;
+}
